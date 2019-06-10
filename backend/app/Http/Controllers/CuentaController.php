13c1621771841f12703a74fe59017f889d5236dd
Rodrigo <rodrigocorderodiaz@gmail.com>
2019-06-09 22:38:04 -0400

    Se implementa verificacion y registro de usuarios

--- backend/app/Http/Controllers/CuentaController.php
+++ backend/app/Http/Controllers/CuentaController.php
@@ -5,19 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-class VerificadorDatorRegistro extends Controller
+class CuentaController extends Controller
 {
     //Metodo que permite el almacenaje de los datos de una nueva cuenta a registrar
     public function nuevoUsuario(Request $request)
     {
         //Variables necesarias que fueron ingresadas
-        $input = $request->only(['correo', 'nombre', 'apellidos', 'contraseña']);
+        $input = $request->only(['correo', 'nombre', 'apellidos', 'contrasena']);
         //Verifica el buen ingreso de datos en los campos requeridos
         $validator = Validator::make($request->all(), [
-            'correo' => 'required|string|max:255',
-            'nombre' => 'required|string|email|max:255|unique:users',
-            'apellidos' => 'required|string|max:255',
-            'contraseña' => 'required|string|max:255',
+            'nombre' => 'required|string|max:255',
+            'correo' => 'required|string|email|max:255|unique:usuarios',
+            'ap_paterno' => 'required|string|max:255',
+            'ap_materno' => 'required|string|max:255',
+            'contrasena' => 'required|string|max:255',
         ]);
         //En caso de una falla en los datos capturados se retorna un codigo de respuesta de error de semantica 
         if ($validator->fails()) {
@@ -25,12 +26,11 @@ class VerificadorDatorRegistro extends Controller
         }
         //Se crea un objeto con los datos capturados
         $nuevoUsuario = new \App\Usuario();
-        $nuevoUsuario->id = $request->id;
         $nuevoUsuario->correo = $request->correo;
         $nuevoUsuario->nombre = $request->nombre;
-        $nuevoUsuario->apellidoPaterno = $request->apellidoPaterno;
-        $nuevoUsuario->apellidoMaterno = $request->apellidoMaterno;
-        $nuevoUsuario->contraseña = $request->contraseña;
+        $nuevoUsuario->ap_paterno = $request->ap_paterno;
+        $nuevoUsuario->ap_materno = $request->ap_materno;
+        $nuevoUsuario->contrasena = \Hash::make($request->contrasena); //Se pone la contraseña en modo cifrado
         //guarda el objeto en la base de datos
         $nuevoUsuario->save();
         //retorna un mensaje informando que la operacion fue exitosa, junto a un codigo de respuesta de peticion completada 
