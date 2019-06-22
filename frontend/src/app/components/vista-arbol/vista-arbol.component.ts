ee9d6d6d7c71523e5d6b391e7008230b1145760c
Matías <palominos90@gmail.com>
2019-06-22 01:35:45 -0400

    Se agregó boceto de cómo se vería el árbol genealógico.

--- frontend/src/app/components/vista-arbol/vista-arbol.component.ts
+++ frontend/src/app/components/vista-arbol/vista-arbol.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-vista-arbol',
@@ -10,6 +10,22 @@ export class VistaArbolComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var ul = document.getElementById("ul-nav");
+    var items = ul.getElementsByTagName("li");
+    var aux;
+    for(var i=0; i<items.length; i++){
+      aux = items[i];
+      aux.setAttribute("class", "invisible");
+    }
+    var li = document.createElement("li");
+    var a = document.createElement("a");
+    var content = document.createTextNode("Cerrar Sesión");
+    li.className = "nav-item";
+    a.className = "nav-link";
+    a.href = "#home";
+    a.appendChild(content);
+    li.appendChild(a);
+    ul.appendChild(li);
   }
 
 }
