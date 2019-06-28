599c39bf8e20ac3248ce99745e716a03a01e264c
Mauricio Herrera <mauricio_hmorales@hotmail.com>
2019-06-27 23:42:25 -0400

    Creación de árbol

--- frontend/src/app/components/vista-arbol/vista-arbol.component.ts
+++ frontend/src/app/components/vista-arbol/vista-arbol.component.ts
@@ -1,15 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-vista-arbol',
   templateUrl: './vista-arbol.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./vista-arbol.component.css']
 })
 export class VistaArbolComponent implements OnInit {
 
+	lista = [{nombre:"perro",hijo:[{nombre:"delfin"}]},{nombre:"gato",hijo:[]}];
   constructor() { }
-
+  testeo= '';
+  boton = '<div><button class="boton">+</button></div>';
+  
   ngOnInit() {
+  console.log(this.lista);
     var ul = document.getElementById("ul-nav");
     var items = ul.getElementsByTagName("li");
     var aux;
@@ -27,5 +32,46 @@ export class VistaArbolComponent implements OnInit {
     li.appendChild(a);
     ul.appendChild(li);
   }
+
+  padre(){
+  	var abc = "<ul><div><button >+</button></div>";
+  	
+  	for (var i = 0 ; i<this.lista.length;i++ ){
+  		
+  		abc = abc + '<li>';
+  		
+  		
+  		abc = abc + '<a>'+this.lista[i].nombre+'</a>';
+
+  		if (this.lista[i].hijo.length > 0){
+  			abc = abc + this.hijo(this.lista[i].hijo[0].nombre);
+  		}
+  		
+  		abc = abc + '</li>';
+  		
+  	}
+  	abc = abc + this.hermano('tapir');
+
+  	this.testeo = abc+'</ul>';
+  	console.log(this.testeo);
+  	return this.testeo ;
+  }
+
+  hermano(nombre){
+  	var abcd = '';
+	    abcd = '<li>';
+		abcd = abcd + '<a>'+nombre+'</a>';
+		abcd = abcd + '</li>';
+		return abcd;
+  }
+  hijo(nombre){
+  	var abcd = '';
+	    abcd = '<ul><li>';
+		abcd = abcd + '<a>'+nombre+'</a>';
+		
+		abcd = abcd + '</li></ul>';
+		return abcd;
+  }
+
 
 }
